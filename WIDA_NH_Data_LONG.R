af8aaf17bb4755e50ebaeac0a31f1348c1dd436b
Damian Betebenner <dbetebenner@nciea.org>
2018-09-07 06:00:44 -0400

    First commit of prep and analysis scripts to repo

---
+++ WIDA_NH_Data_LONG.R
@@ -0,0 +1,68 @@
+#################################################################################
+###
+### Data preparation script for WIDA NH data, 2017 & 2018
+###
+#################################################################################
+
+### Load Packages
+
+require(SGP)
+require(data.table)
+
+
+### Utility function
+
+strtail <- function (s, n = 1) {
+    if (n < 0)
+        substring(s, 1 - n)
+    else substring(s, nchar(s) - n + 1)
+}
+
+strhead <- function (s, n) {
+    if (n < 0)
+        substr(s, 1, nchar(s) + n)
+    else substr(s, 1, n)
+}
+
+
+### Load Data
+
+WIDA_NH_Data_LONG_2017 <- fread("Data/Base_Files/ESOLDatafor2017.dat", colClasses=rep("character", 5))
+WIDA_NH_Data_LONG_2018 <- fread("Data/Base_Files/ESOLDatafor2018.dat", colClasses=rep("character", 5))
+WIDA_NH_Data_LONG <- rbindlist(list(WIDA_NH_Data_LONG_2017, WIDA_NH_Data_LONG_2018))
+
+
+### Clean Up Data
+
+WIDA_NH_Data_LONG <- WIDA_NH_Data_LONG[!ID=="NULL"]
+setnames(WIDA_NH_Data_LONG, c("YEAR", "ID", "GRADE", "SCALE_SCORE", "ACHIEVEMENT_LEVEL_ORIGINAL"))
+WIDA_NH_Data_LONG[,GRADE:=as.character(as.numeric(GRADE))]
+WIDA_NH_Data_LONG[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+WIDA_NH_Data_LONG[,ACHIEVEMENT_LEVEL_ORIGINAL:=strhead(paste0(ACHIEVEMENT_LEVEL_ORIGINAL, ".0"), 3)]
+WIDA_NH_Data_LONG[,ACHIEVEMENT_LEVEL := ACHIEVEMENT_LEVEL_ORIGINAL]
+WIDA_NH_Data_LONG[,ACHIEVEMENT_LEVEL := strhead(ACHIEVEMENT_LEVEL, 1)]
+WIDA_NH_Data_LONG[ACHIEVEMENT_LEVEL_ORIGINAL %in% c("4.2", "4.3", "4.4"), ACHIEVEMENT_LEVEL:="4.2"]
+WIDA_NH_Data_LONG[ACHIEVEMENT_LEVEL_ORIGINAL %in% c("4.5", "4.6", "4.7", "4.8", "4.9"), ACHIEVEMENT_LEVEL:="4.5"]
+WIDA_NH_Data_LONG[!is.na(ACHIEVEMENT_LEVEL),ACHIEVEMENT_LEVEL := paste("WIDA Level", ACHIEVEMENT_LEVEL)]
+WIDA_NH_Data_LONG[,VALID_CASE := "VALID_CASE"]
+WIDA_NH_Data_LONG[,CONTENT_AREA := "READING"]
+
+
+
+
+### Check for duplicates
+
+setkey(WIDA_NH_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, ID, GRADE, SCALE_SCORE)
+setkey(WIDA_NH_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, ID)
+WIDA_NH_Data_LONG[which(duplicated(WIDA_NH_Data_LONG, by=key(WIDA_NH_Data_LONG)))-1, VALID_CASE := "INVALID_CASE"]
+setkey(WIDA_NH_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, ID)
+
+
+### Reorder
+
+setcolorder(WIDA_NH_Data_LONG, c(7,8,1,2,3,4,6,5))
+
+
+### Save data
+
+save(WIDA_NH_Data_LONG, file="Data/WIDA_NH_Data_LONG.Rdata")
---
+++ WIDA_NH_SGP.R
@@ -0,0 +1,39 @@
+##########################################################################################
+###
+### Script for calculating SGPs for 2017-2018 WIDA/ACCESS Indiana
+###
+##########################################################################################
+
+### Load SGP package
+
+require(SGP)
+options(error=recover)
+#options(warn=2)
+
+
+### Load Data
+
+load("Data/WIDA_NH_Data_LONG.Rdata")
+
+
+### Run analyses
+
+WIDA_NH_SGP <- abcSGP(
+		WIDA_NH_Data_LONG,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "visualizeSGP", "outputSGP"),
+		sgp.percentiles=TRUE,
+		sgp.projections=TRUE,
+		sgp.projections.lagged=TRUE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
+		get.cohort.data.info=TRUE,
+		sgp.target.scale.scores=TRUE,
+		plot.types=c("growthAchievementPlot", "studentGrowthPlot"),
+		sgPlot.demo.report=TRUE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4, GA_PLOTS=1, SG_PLOTS=1)))
+
+
+### Save results
+
+save(WIDA_NH_SGP, file="Data/WIDA_NH_SGP.Rdata")
