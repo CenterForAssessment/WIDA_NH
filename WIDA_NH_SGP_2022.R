26d9048dc740cb475701a9f3da3ab35de7a7079a
Damian Betebenner <dbetebenner@nciea.org>
2022-07-19 20:41:03 -0400

    Adding 2022 SGP scripts

---
+++ WIDA_NH_SGP_2022.R
@@ -0,0 +1,36 @@
+##########################################################################################
+###
+### Script for calculating SGPs for 2021-2022 WIDA/ACCESS New Hampshire
+###
+##########################################################################################
+
+### Load SGP package
+require(SGP)
+
+### Load Data
+load("Data/WIDA_NH_SGP.Rdata")
+load("Data/WIDA_NH_Data_LONG_2022.Rdata")
+
+###   Add single-cohort baseline matrices to SGPstateData
+SGPstateData <- SGPmatrices::addBaselineMatrices("WIDA", "2022", "WIDA_NH")
+
+### Run updateSGP
+WIDA_NH_SGP <- updateSGP(
+		WIDA_NH_SGP,
+		WIDA_NH_Data_LONG_2022,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "visualizeSGP", "outputSGP"),
+		sgp.percentiles=TRUE,
+		sgp.projections=TRUE,
+		sgp.projections.lagged=TRUE,
+		sgp.percentiles.baseline=TRUE,
+		sgp.projections.baseline=TRUE,
+		sgp.projections.lagged.baseline=TRUE,
+		get.cohort.data.info=TRUE,
+		sgp.target.scale.scores=TRUE,
+		plot.types=c("growthAchievementPlot", "studentGrowthPlot"),
+		sgPlot.demo.report=TRUE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4, GA_PLOTS=1, SG_PLOTS=1)))
+
+
+### Save results
+#save(WIDA_NH_SGP, file="Data/WIDA_NH_SGP.Rdata")
