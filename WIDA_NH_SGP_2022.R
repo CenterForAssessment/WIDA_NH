961d849a21552615d6d19fbde769a21bdcb87d31
Damian Betebenner <dbetebenner@nciea.org>
2022-10-18 09:49:46 -0400

    Update of 2022 script with additional students merged in

--- WIDA_NH_SGP_2022.R
+++ WIDA_NH_SGP_2022.R
@@ -6,18 +6,29 @@
 
 ### Load SGP package
 require(SGP)
+require(data.table)
 
 ### Load Data
 load("Data/WIDA_NH_SGP.Rdata")
 load("Data/WIDA_NH_Data_LONG_2022.Rdata")
+load("Data/Base_Files/WIDA_NH_ID_GRADE_REASSIGN.Rdata")
+
+### Merge old and new data using updateSGP 
+WIDA_NH_SGP <- updateSGP(WIDA_NH_SGP, WIDA_NH_Data_LONG_2022, steps="prepareSGP")
+
+### Update GRADE to accomodate repeaters in analyses
+setkey(WIDA_NH_SGP@Data, VALID_CASE, CONTENT_AREA, YEAR, ID)
+setkey(WIDA_NH_ID_GRADE_REASSIGN, VALID_CASE, CONTENT_AREA, YEAR, ID)
+WIDA_NH_SGP@Data[,GRADE_ORIGINAL:=GRADE]
+WIDA_NH_SGP@Data[WIDA_NH_ID_GRADE_REASSIGN[,c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"), with=FALSE], GRADE:=WIDA_NH_ID_GRADE_REASSIGN$GRADE_NEW]
 
 ###   Add single-cohort baseline matrices to SGPstateData
 SGPstateData <- SGPmatrices::addBaselineMatrices("WIDA", "2022", "WIDA_NH")
 
 ### Run updateSGP
-WIDA_NH_SGP <- updateSGP(
+WIDA_NH_SGP <- abcSGP(
 		WIDA_NH_SGP,
-		WIDA_NH_Data_LONG_2022,
+		years="2022",
 		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "visualizeSGP", "outputSGP"),
 		sgp.percentiles=TRUE,
 		sgp.projections=TRUE,
@@ -33,4 +44,4 @@ WIDA_NH_SGP <- updateSGP(
 
 
 ### Save results
-save(WIDA_NH_SGP, file="Data/WIDA_NH_SGP.Rdata")
+#save(WIDA_NH_SGP, file="Data/WIDA_NH_SGP.Rdata")
